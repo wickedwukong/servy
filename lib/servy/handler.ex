15eeba9a7b6eb335ab7c36a1f61a8d4b4c0d154e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-02 21:03:34 +0000

    a catach all and return 404 function

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,7 +13,10 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method,
+      path: path,
+      resp_body: "",
+      status: nil}
   end
 
   def route(request_map) do
@@ -22,20 +25,35 @@ defmodule Servy.Handler do
 
   def route(request_map, "GET", "/wildthings") do
     # Map.put(request_map, :resp_body, "Bears, Lions, Tigers")
-    %{request_map | resp_body: "Bears, Lions, Tigers"}
+    %{request_map | resp_body: "Bears, Lions, Tigers", status: 200}
   end
 
   def route(request_map, "GET", "/bears") do
-    %{request_map | resp_body: "Paddington, Smokey, Teddy"}
+    %{request_map | resp_body: "Paddington, Smokey, Teddy", status: 200}
+  end
+
+  def route(request_map, _, path) do
+    %{request_map | resp_body: "No resource found at #{path}", status: 404}
   end
 
   def format_response(resposne_map) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{resposne_map.status} OK
     Content-Type: text/html
     Content-Length: #{String.length(resposne_map.resp_body)}
 
     #{resposne_map.resp_body}
     """
+  end
+
+  defp status_descrition(status) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
