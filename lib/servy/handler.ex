1b805111dfe1492d9b07233f31b272984d2e3037
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-02 15:42:11 +0000

    route and format message

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,9 +6,9 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def parse(rawRequest) do
+  def parse(raw_request) do
     [method, path|_] =
-      rawRequest
+      raw_request
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
@@ -16,17 +16,17 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: ""}
   end
 
-  def route(requestMap) do
-    %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  def route(request_map) do
+    # Map.put(request_map, :resp_body, "Bears, Lions, Tigers")
+    %{request_map | resp_body: "Bears, Lions, Tigers"}
   end
 
-  def format_response(resposneMap) do
+  def format_response(resposne_map) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(resposne_map.resp_body)}
 
-    Bears, Lions, Tigers
     """
   end
 end
