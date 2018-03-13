9a15e916c1ff7306fc3ef5c7ef6638de57f99a5e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-13 16:59:12 +0000

    use Task instead of hand rolled linking process

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -40,13 +40,13 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
     snapshots =
       ["cam-1", "cam-2","cam-3"]
-      |> Enum.map(&Fetcher.async(fn -> Servy.VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Fetcher.get_result(&1))
+      |> Enum.map(&Task.async(fn -> Servy.VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await(&1))
 
-    pid4 = Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
-    location = Fetcher.get_result(pid4)
+    location = Task.await(task)
 
     %{conv | status: 200, resp_body: inspect {snapshots, location}}
   end
