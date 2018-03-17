acf43cf18345e696e2c8a70f58aa39ccb1e64876
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-17 22:27:14 +0000

    use cache size for Emum.take

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -17,7 +17,7 @@ defmodule Servy.PledgeServer do
 
   def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} =send_pledge_to_service(name, amount)
-    most_recent_pledges = Enum.take(state.pledges, 2)
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
     new_state = %{state | pledges: [{name, amount} | most_recent_pledges]}
     {:reply, id, new_state}
   end
