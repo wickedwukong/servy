ac74badb09daaa13ef91cd71bd84734f6e6ba430
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-18 22:15:25 +0000

    link http server process to kick starter process. The http server crash kills kick starter process too

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,15 +1,16 @@
 defmodule Servy.KickStarter do
   use GenServer
 
-  @name :http_server
-
   def start do
+    IO.puts "Starting kick starter....."
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(state) do
+    IO.puts "Starting http server"
     http_server_pid = spawn(Servy.HttpServer, :start, [4000])
-    Process.register(http_server_pid, @name)
+    Process.link(http_server_pid)
+    Process.register(http_server_pid, :http_server)
 
     {:ok, http_server_pid}
   end
