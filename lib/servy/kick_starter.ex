944884da8164ea90f68e6213e38f940676c28119
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-18 22:38:18 +0000

    handle crash in handle info

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -6,13 +6,23 @@ defmodule Servy.KickStarter do
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
-  def init(state) do
-    IO.puts "Starting http server"
+  def init(:ok) do
     Process.flag(:trap_exit, true)
-    http_server_pid = spawn(Servy.HttpServer, :start, [4000])
+    http_server_pid = start_server()
+    {:ok, http_server_pid}
+  end
+
+  def handle_info({:EXIT, _child_process_pid, message}, _state) do
+    IO.puts "exit signal received: #{inspect message}"
+    new_http_server_pid = start_server()
+    {:noreply, new_http_server_pid}
+  end
+
+  defp start_server do
+    IO.puts "Starting http server"
+    http_server_pid = spawn_link(Servy.HttpServer, :start, [4000])
     Process.link(http_server_pid)
     Process.register(http_server_pid, :http_server)
-
-    {:ok, http_server_pid}
+    http_server_pid
   end
 end
