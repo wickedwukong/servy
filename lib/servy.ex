41e8402ca5d73669fb97519762dfd5f72b17a7d4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-03-22 21:11:59 +0000

    automatically start servy when start up the application

--- lib/servy.ex
+++ lib/servy.ex
@@ -1,6 +1,13 @@
 defmodule Servy do
+  use Application
+
   def hello(name) do
     "Hello #{name}"
+  end
+
+  def start(_type, _arg) do
+    IO.puts "Starting the #{__MODULE__} application..."
+    Servy.Supervisor.start_link()
   end
 end
 
